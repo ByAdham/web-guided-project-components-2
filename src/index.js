91111aa562fb595efd1bd4f0f2aab2236bad0e46
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-01 19:18:18 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -15,6 +15,9 @@ axios.get('https://lambda-times-api.herokuapp.com/friends')
     console.log(drama)
   })
 
+// if you want to handle several independent promises
+// Promise.all().then()
+
 // the future data is NOT ins this area of the program
 console.log('3 We requested data with axios')
 
